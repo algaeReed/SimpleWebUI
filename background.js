dd102097b4e46f8d0a4cc7005adda438f4aa0cfe
dearzhe <30750777+dearzhe@users.noreply.github.com>
2019-09-11 14:51:14 +0800

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// 当插件图标被点击后，将调用这个方法
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    // 运行 run.js 中的代码
-    chrome.tabs.executeScript( activeTab.id , {file:'run.js'});
-});
-
